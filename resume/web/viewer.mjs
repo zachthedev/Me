e3d53fadbf438ab3bb0ef99d8c66e1db7609b7e1
ZachTheDev <zach@kodebalt.com>
2024-03-28 22:06:25 -0500

    Changed worker

--- resume/web/viewer.mjs
+++ resume/web/viewer.mjs
@@ -4518,7 +4518,7 @@ const defaultOptions = {
 };
 {
   defaultOptions.defaultUrl = {
-    value: "https://corsproxy.kodebalt.workers.dev/https://drive.google.com/uc?id=1x_OPj1eTVYjztjKfSYKhUFhH4fXpsrF9",
+    value: "https://fetchdrivepdf.kodebalt.workers.dev/?fileId=1x_OPj1eTVYjztjKfSYKhUFhH4fXpsrF9",
     kind: OptionKind.VIEWER
   };
   defaultOptions.sandboxBundleSrc = {
